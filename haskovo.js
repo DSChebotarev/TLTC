01aa120d220f692532dec831bbe3ea27e3dc1639
Alex Stoyanov <93249171+ASStoyanov20@users.noreply.github.com>
2022-03-20 20:47:27 +0200

    added 70% of the cities in Bulgaria

---
+++ haskovo.js
@@ -0,0 +1,29 @@
+const btn = document.querySelector(".btn");
+const btn1 = document.querySelector(".btn1");
+const btnText = document.querySelector(".buttonText");
+const text1 = document.querySelector(".text");
+const wrong = document.querySelector(".wrong");
+const tryAgain = document.querySelector(".tryAgain");
+
+function code() {
+    const codeOption = Number(prompt("Enter a number"));
+    
+    if(codeOption == 6012) {
+        btn.classList.remove("hidden");
+        btn1.classList.remove("hidden");
+        btnText.classList.remove("hidden");
+        
+    } else {
+        wrong.classList.remove("hidden");
+        tryAgain.classList.remove("hidden");
+    }
+}
+
+code();
+
+btn.addEventListener("click", function () {
+    text1.classList.remove("hidden");
+    btn.classList.add("hidden");
+    btn1.classList.add("hidden");
+    btnText.classList.add("hidden");
+})
